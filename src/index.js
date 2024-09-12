44e0ceed5e3cbb4320b5f7c5075f43a077ad6bee
Henry Clyde <22669531+henryclyde@users.noreply.github.com>
2024-09-12 02:32:24 -0600

    added click events for page buttons, building html outline with javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,50 @@
-console.log("hello world");
+
+
+let content = document.getElementById("content");
+while(content.firstChild) {
+    content.removeChild(content.firstChild);
+}
+
+let home = document.getElementById("home");
+let menu = document.getElementById("menu");
+let about = document.getElementById("about");
+
+//Build Home page
+home.addEventListener("click", () => {
+        while(content.firstChild) {
+            content.removeChild(content.firstChild);
+        }
+        let title = document.createElement("h1");
+        title.textContent = "Welcome to Athen's Garden";
+        content.appendChild(title);
+        let description = document.createElement("p");
+        description.textContent = "Athen's Garden provides a variety of dishes, from ancient delicassies to modern favorites, our menu boasts a healthy spectrum of mediteranean cuisine.";
+        content.appendChild(description); 
+});
+
+//Build Menu
+menu.addEventListener("click", () => {
+    while(content.firstChild) {
+        content.removeChild(content.firstChild);
+    }
+    let title = document.createElement("h1");
+    title.textContent = "Welcome to Athen's Garden";
+    content.appendChild(title);
+    let description = document.createElement("p");
+    description.textContent = "Athen's Garden provides a variety of dishes, from ancient delicassies to modern favorites, our menu boasts a healthy spectrum of mediteranean cuisine.";
+    content.appendChild(description); 
+});
+
+//Build About page
+about.addEventListener("click", () => {
+    while(content.firstChild) {
+        content.removeChild(content.firstChild);
+    }
+    let title = document.createElement("h1");
+    title.textContent = "Welcome to Athen's Garden";
+    content.appendChild(title);
+    let description = document.createElement("p");
+    description.textContent = "Athen's Garden provides a variety of dishes, from ancient delicassies to modern favorites, our menu boasts a healthy spectrum of mediteranean cuisine.";
+    content.appendChild(description); 
+});
+
